51cbed5e7358c649f1b613925f6b0485045ef5ee
sam <sublimecraftftw@gmail.com>
2023-09-10 21:46:00 -0700

    Room join link

---
+++ src/pages/join/[id].tsx
@@ -0,0 +1,69 @@
+import type { GetServerSideProps } from "next";
+import { getServerAuthSession } from "~/server/auth";
+import { prisma } from "~/server/db";
+
+export const getServerSideProps: GetServerSideProps<
+  never,
+  { id: string }
+> = async (context) => {
+  if (!context.params) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: true,
+      },
+    };
+  }
+
+  const { id } = context.params;
+
+  const session = await getServerAuthSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    };
+  }
+
+  const room = await prisma.room.findUnique({
+    where: {
+      slug: id,
+    },
+  });
+
+  if (!room) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    };
+  }
+
+  await prisma.user.update({
+    where: {
+      id: session.user.id,
+    },
+    data: {
+      currentRoom: {
+        connect: {
+          id: room.id,
+        },
+      },
+    },
+  });
+
+  return {
+    redirect: {
+      destination: `/room/${room.slug}`,
+      permanent: true,
+    },
+  };
+};
+
+export default function JoinPage() {
+  return null;
+}

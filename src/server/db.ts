74fa6846993b4ec3254305049216baa1f6c4c6a9
sam <sublimecraftftw@gmail.com>
2024-03-19 20:48:26 -0700

    Update prisma + use edge supported stuff

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { Client } from '@planetscale/database'
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 
 import { env } from "~/env.mjs";
+
+const client = new Client({
+  url: env.DATABASE_URL,
+});
+const adapter = new PrismaPlanetScale(client)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -9,6 +16,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
